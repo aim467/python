3f4bf67a68f1915a056ee9e77a89c734a9c90e88
aim467 <709544711@qq.com>
2018-06-10 14:34:12 +0800

    优化效率

--- ipSearch.py
+++ ipSearch.py
@@ -2,20 +2,26 @@ import requests
 import json
 
 
-input_ip = input("请输入你要查询的ip:")
 
-    
-url = 'http://ip.taobao.com/service/getIpInfo.php?ip='+input_ip
-data = requests.get(url)
-#print(data.status_code)
-#print(data.encoding)
 
-ip = data.json()["data"]["ip"]
-isp = data.json()["data"]["isp"]
-country = data.json()["data"]["country"]
-city = data.json()["data"]["city"]
+def getIp(input_ip):
+    url = 'http://ip.taobao.com/service/getIpInfo.php?ip='+input_ip
 
-print("你所查询的ip为:"+ip)
-print("你所查询的ip的服务商为:"+isp)
-print("你所查询的ip的国家为:"+country)
-print("你所查询的ip的城市为:"+city)  
+    data = requests.get(url)
+    #data.raise_for_status()
+    #data.encode = data.apparent_encoding
+    ip = data.json()["data"]["ip"]
+    isp = data.json()["data"]["isp"]
+    country = data.json()["data"]["country"]
+    city = data.json()["data"]["city"]
+
+    print("你所查询的ip为:"+ip)
+    print("你所查询的ip的服务商为:"+isp)
+    print("你所查询的ip的国家为:"+country)
+    print("你所查询的ip的城市为:"+city)  
+
+
+if __name__ == '__main__':
+    while True:
+        input_ip = input("请输入你要查询的ip:")
+        getIp(input_ip) 
--- weather.py
+++ weather.py
@@ -1,28 +1,32 @@
 import requests
 import json
 
-cityname = input("请输入你要查询的城市名:")
 
-url = 'https://www.sojson.com/open/api/weather/json.shtml?city='+cityname
+def weather(cityname):
+    url = 'https://www.sojson.com/open/api/weather/json.shtml?city='+cityname
+    data = requests.get(url)
 
-data = requests.get(url)
-'''
-data.json
-city = data.json()["city"]
-wendu = data.json()["data"]["wendu"]
-wuran = data.json()["data"]["quality"]
+    '''
+    data.json
+    city = data.json()["city"]
+    wendu = data.json()["data"]["wendu"]
+    wuran = data.json()["data"]["quality"]
+    print("你所查询的城市为:"+city)
+    print("你所查询的城市今天的温度为:"+wendu+"℃")
+    print("污染级数为:"+wuran)
+    '''
 
-print("你所查询的城市为:"+city)
-print("你所查询的城市今天的温度为:"+wendu+"℃")
-print("污染级数为:"+wuran)
-'''
-date = data.json()["data"]["forecast"]
+    date = data.json()["data"]["forecast"]
+    from tabulate import tabulate
+    for dict1 in date[:4]:
+        dict2 = dict1.keys()
+        dict3 = dict1.values()
+        list = []
+        list.append(dict2)
+        list.append(dict3)
+        print(tabulate(list,tablefmt='grid'))
+        print('\n')   
 
-from tabulate import tabulate
-for dict1 in date[:4]:
-    dict2 = dict1.keys()
-    dict3 = dict1.values()
-    list = []
-    list.append(dict2)
-    list.append(dict3)
-    print(tabulate(list,tablefmt='grid'))   
+if __name__ == '__main__':
+    cityname = input("请输入你要查询的城市名:")
+    weather(str(cityname))
